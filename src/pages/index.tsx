c8bde07f52a4637486a1976610dee578f3272ff5
vedikaparvez <ved.parvez@gmail.com>
2023-07-31 18:22:13 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function HomePage() {
         <meta name="description" content="Vedika Parvez" />
       </Head>
       <HomePageHeroSection />
+      <iframe id="softr-8f54cdd5-557e-4402-891f-cdaf6b4de64f-hero3" src="https://www.vedikaparvez.com/embed/pages/8f54cdd5-557e-4402-891f-cdaf6b4de64f/blocks/hero3" width="100%" height="1000" scrolling="no" frameborder="0" style="border:none;"></iframe>    <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.11/iframeResizer.min.js"></script>    <script> iFrameResize({ checkOrigin: false, log: true }, '#softr-8f54cdd5-557e-4402-891f-cdaf6b4de64f-hero3');</script>
     </>
   )
 }
